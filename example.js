00b8d5e70ea5ad88d611362891e5cec3362a0a07
William Katz <qualitycontentquestionmark@gmail.com>
2021-08-04 02:24:46 -0700

    updated

--- example.js
+++ example.js
@@ -8,21 +8,26 @@ function resize(arr, newSize, defaultValue) {
 }
 
 Matrix().then(function(Module) { 
-    let a = new Module.mat_i32(1000, 1000);
-    let c = [];
-    let sz = a.area();
-    for(let i = 0; i < sz; i++) {
-        a.set_fast_at(i, Math.random()*100);
-        c.push(Math.random()*100);
-    }
+    // let a = new Module.mat_f64(1000, 1000);
+    // let c = new Array(1000000);
+    // let sz = a.area();
+    // for(let i = 0; i < sz; i++) {
+    //     a.set_fast_at(i, Math.random()*100);
+    //     c.push(Math.random()*100);
+    // }
     let sum_a = 0, sum_b = 0;
     for(let i = 0; i < 10; i++) {
-        let b = new Module.mat_i32(1000, 1000);
-        let d = [];
+        // let b = new Module.mat_i32(1000, 1000);
+        // let d = [];
         
         {
+            //0.78x the speed
             let t0 = performance.now()
+            // let d = c.slice();
+            // var d = new Array(1000000);
+            // d.length;
             // resize(d, 4000000, 0);
+            // resize(d, 250, 0);
             // for(let j = 0; j < sz; j++) {
                 // d[j] = d[j];
                 // d[j] = 10;
@@ -33,7 +38,11 @@ Matrix().then(function(Module) {
         }
         {
             let t0 = performance.now()
+            // let b = new Module.mat_f64(a);
+            // var a = new Module.mat_i32(1000, 1000);
+            // b.area()
             // b.resize(2000, 2000);
+            // b.resize(50, 50);
             // Module.copy_mat_i32_i32(a, b);
             // Module.fill_mat_i32_i32(a, 10);
             // Module.sort_mat_i32(a);
