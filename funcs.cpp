43d2621e433461672226fe1827cd631ce594f868
William Katz <qualitycontentquestionmark@gmail.com>
2021-08-03 23:06:53 -0700

    :art: design changes

--- funcs.cpp
+++ funcs.cpp
@@ -25,44 +25,56 @@ using namespace mat;
 EMSCRIPTEN_BINDINGS(module)
 {
     MAT_GEN(mat_i32, int32_t)
-    function("fill_mat_i32", &Fill<hMat<int32_t>, int32_t>);
+    function("fill_mat_i32_i32", &Fill<hMat<int32_t>, int32_t>);
+    function("fill_mat_i32_i64", &Fill<hMat<int32_t>, int64_t>);
+    function("fill_mat_i32_float", &Fill<hMat<int32_t>, float>); 
+    function("fill_mat_i32_f64", &Fill<hMat<int32_t>, double>);
 
     function("copy_mat_i32_i32", &Copy<hMat<int32_t>>); //self
-    function("copy_mat_i32_ui32", &Copy<hMat<int32_t>, hMat<uint32_t>>);
-    function("copy_mat_i32_bool", &Copy<hMat<int32_t>, hMat<bool>>);
+    function("copy_mat_i32_i64", &Copy<hMat<int32_t>, hMat<int64_t>>);
     function("copy_mat_i32_float", &Copy<hMat<int32_t>, hMat<float>>);
+    function("copy_mat_i32_f64", &Copy<hMat<int32_t>, hMat<double>>);
 
     function("sort_mat_i32", &SortMut<hMat<int32_t>>);
 
-    MAT_GEN(mat_ui32, uint32_t)
-    function("fill_mat_ui32", &Fill<hMat<uint32_t>, uint32_t>);
+    MAT_GEN(mat_i64, int64_t)
+    function("fill_mat_i64_i32", &Fill<hMat<int32_t>, int32_t>);
+    function("fill_mat_i64_i64", &Fill<hMat<int32_t>, int64_t>);
+    function("fill_mat_i64_float", &Fill<hMat<int32_t>, float>); 
+    function("fill_mat_i64_f64", &Fill<hMat<int32_t>, double>);
 
-    function("copy_mat_ui32_i32", &Copy<hMat<uint32_t>, hMat<int32_t>>);
-    function("copy_mat_ui32_ui32", &Copy<hMat<uint32_t>>); //self
-    function("copy_mat_ui32_bool", &Copy<hMat<uint32_t>, hMat<bool>>);
-    function("copy_mat_ui32_float", &Copy<hMat<uint32_t>, hMat<float>>);
+    function("copy_mat_i64_i32", &Copy<hMat<int64_t>, hMat<int32_t>>);
+    function("copy_mat_i64_i64", &Copy<hMat<int64_t>>); //self
+    function("copy_mat_i64_float", &Copy<hMat<int64_t>, hMat<float>>);
+    function("copy_mat_i64_f64", &Copy<hMat<int64_t>, hMat<double>>);
 
     function("sort_mat_i32", &SortMut<hMat<int32_t>>);
 
-    MAT_GEN(mat_bool, bool)
-    function("fill_mat_bool", &Fill<hMat<bool>, bool>);
+    MAT_GEN(mat_f32, float)
+    function("fill_mat_float_i32", &Fill<hMat<float>, int32_t>);
+    function("fill_mat_float_i64", &Fill<hMat<float>, int64_t>);
+    function("fill_mat_float_float", &Fill<hMat<float>, float>); 
+    function("fill_mat_float_f64", &Fill<hMat<float>, double>);
 
-    function("copy_mat_bool_i32", &Copy<hMat<bool>, hMat<int32_t>>);
-    function("copy_mat_bool_ui32", &Copy<hMat<bool>, hMat<uint32_t>>);
-    function("copy_mat_bool_bool", &Copy<hMat<bool>>); //self
-    function("copy_mat_bool_float", &Copy<hMat<bool>, hMat<float>>);
+    function("copy_mat_float_i32", &Copy<hMat<float>, hMat<int32_t>>);
+    function("copy_mat_float_i64", &Copy<hMat<float>, hMat<int64_t>>);
+    function("copy_mat_float_float", &Copy<hMat<float>>); //self
+    function("copy_mat_float_f64", &Copy<hMat<float>, hMat<double>>);
 
-    function("sort_mat_bool", &SortMut<hMat<bool>>);
+    function("sort_mat_float", &SortMut<hMat<float>>);
 
-    MAT_GEN(mat_float, double)
-    function("fill_mat_float", &Fill<hMat<double>, double>);
+    MAT_GEN(mat_f64, double)
+    function("fill_mat_f64_i32", &Fill<hMat<double>, int32_t>);
+    function("fill_mat_f64_i64", &Fill<hMat<double>, int64_t>);
+    function("fill_mat_f64_float", &Fill<hMat<double>, float>); 
+    function("fill_mat_f64_f64", &Fill<hMat<double>, double>);
+    
+    function("copy_mat_f64_i32", &Copy<hMat<double>, hMat<int32_t>>);
+    function("copy_mat_f64_i64", &Copy<hMat<double>, hMat<int64_t>>);
+    function("copy_mat_f64_float", &Copy<hMat<double>, hMat<float>>);
+    function("copy_mat_f64_f64", &Copy<hMat<double>>); //self
 
-    function("copy_mat_float_i32", &Copy<hMat<double>, hMat<int32_t>>);
-    function("copy_mat_float_ui32", &Copy<hMat<double>, hMat<uint32_t>>);
-    function("copy_mat_float_bool", &Copy<hMat<double>, hMat<bool>>);
-    function("copy_mat_float_float", &Copy<hMat<double>>); //self
-
-    function("sort_mat_float", &SortMut<hMat<double>>);
+    function("sort_mat_f64", &SortMut<hMat<double>>);
 }
 
 // em++ --bind include/funcs.cpp -O3 -fno-exceptions -fno-rtti -DEMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s WASM_BIGINT --closure 1 -s ASSERTIONS=0 -s MODULARIZE=1 -s 'EXPORT_NAME="Matrix"'
