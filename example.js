cba7b5accf5afd396b9227c183ac984ff8eaa37c
William Katz <qualitycontentquestionmark@gmail.com>
2021-08-09 17:44:35 -0700

    Update example.js

--- example.js
+++ example.js
@@ -21,23 +21,23 @@ Matrix().then(function(Module) {
         // let d = new Array(1000000);
         {
             let t0 = performance.now()
-            for(let i = 0; i < sz; i++) {
-                c[i] = Math.random()*100;
-            }
+            // for(let i = 0; i < sz; i++) {
+            //     c[i] = Math.random()*100;
+            // }
             // let d = c.slice();
             // var d = new Array(1000000);
             // d.length;
             // resize(d, 4000000, 0);
             // resize(d, 250, 0);
             // let d = c.slice();
-            // d.fill(10);
+            // c.fill(10);
             // c.sort();
             let t1 = performance.now()
             sum_a += (t1 - t0);
         }
         {
             let t0 = performance.now()
-            Module.randfill_mat_i32(a, 0, 100)
+            // Module.randfill_mat_i32(a, 0, 100)
             // Module.copy_mat_f64_f64(a, b);
             // let b = new Module.mat_f64(a);
             // var a = new Module.mat_i32(1000, 1000);
@@ -45,7 +45,7 @@ Matrix().then(function(Module) {
             // b.resize(2000, 2000);
             // b.resize(50, 50);
             // Module.copy_mat_i32_i32(a, b);
-            // Module.fill_mat_f64_f64(b, 10);
+            // Module.fill_mat_i32(a, 10);
             // Module.sort_mat_i32(a);
             let t1 = performance.now()
             sum_b += (t1 - t0);
