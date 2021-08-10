d5707222864d90832be06d73b6fdec96a3294d78
William Katz <qualitycontentquestionmark@gmail.com>
2021-08-09 20:23:57 -0700

    Update example.js

--- example.js
+++ example.js
@@ -8,16 +8,16 @@ const Matrix = require('./wmat.js')
 // }
 
 Matrix().then(function(Module) { 
-    let a = new Module.mat_i32(1000, 1000);
-    let c = new Array(1000000);
-    let sz = a.area();
+    // let a = new Module.mat_f64(1000, 1000);
+    // let c = new Array(1000000);
+    // let sz = a.area();
     // for(let i = 0; i < sz; i++) {
     //     a.set_fast_at(i, Math.random()*100);
     //     c.push(Math.random()*100);
     // }
     let sum_a = 0, sum_b = 0;
     for(let i = 0; i < 10; i++) {
-        // let b = new Module.mat_f64(1000, 1000);
+        // let b = new Module.mat_f32(1000, 1000);
         // let d = new Array(1000000);
         {
             let t0 = performance.now()
@@ -26,9 +26,9 @@ Matrix().then(function(Module) {
             // }
             // let d = c.slice();
             // var d = new Array(1000000);
-            // d.length;
-            // resize(d, 4000000, 0);
-            // resize(d, 250, 0);
+            // c.length;
+            // resize(c, 4000000, 0);
+            // resize(c, 250, 0);
             // let d = c.slice();
             // c.fill(10);
             // c.sort();
@@ -37,19 +37,19 @@ Matrix().then(function(Module) {
         }
         {
             let t0 = performance.now()
-            // Module.randfill_mat_i32(a, 0, 100)
-            // Module.copy_mat_f64_f64(a, b);
-            // let b = new Module.mat_f64(a);
-            // var a = new Module.mat_i32(1000, 1000);
-            // b.area()
-            // b.resize(2000, 2000);
-            // b.resize(50, 50);
+            // Module.randfill_mat_f32(a, 0, 100)
+            // Module.copy_mat_f32_f32(a, b);
+            // let b = new Module.mat_f32(a);
+            // var a = new Module.mat_f32(1000, 1000);
+            // a.area()
+            // a.resize(2000, 2000);
+            // a.resize(50, 50);
             // Module.copy_mat_i32_i32(a, b);
-            // Module.fill_mat_i32(a, 10);
-            // Module.sort_mat_i32(a);
+            // Module.fill_mat_f64(a, 10);
+            // Module.sort_mat_f32(a);
             let t1 = performance.now()
             sum_b += (t1 - t0);
         }
     }
-    console.log(sum_a/sum_b );    
+    console.log((sum_a/sum_b)  );    
 });
